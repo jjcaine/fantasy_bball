c91623375b25377bc0ffb9160a21741dd7c3f530
John Caine <jjcaine@github.com>
2019-09-11 07:56:15 -0400

    user log in and log out working along with a rudimentary profile

--- fantasy/fantasy/settings.py
+++ fantasy/fantasy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'draft.apps.DraftConfig',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -123,6 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- fantasy/fantasy/urls.py
+++ fantasy/fantasy/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from draft import views as draft_views
+from users import views as users_views
 
 urlpatterns = [
     path('', draft_views.index, name='index'),
@@ -33,6 +35,12 @@ urlpatterns = [
     path('create_draft', draft_views.create_draft, name='create_draft'),
     path('draft_configuration_home/<int:draft_id>', draft_views.draft_configuration_home, name='draft_configuration_home'),
     path('configure_projection_columns/<int:draft_id>', draft_views.configure_projection_columns, name='configure_projection_columns'),
+
+    # Begin Users URLs
+    path('register', users_views.register, name='register'),
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile', users_views.profile, name='profile'),
 ]
 
 if settings.DEBUG:
